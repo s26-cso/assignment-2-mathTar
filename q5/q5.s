14cf7a7e6999fcb5fdcdd60086ded8a448e1265a
Tarun Hariharan <tarun18hari@gmail.com>
2026-04-05 01:19:15 +0530

    changed from syscalls to libc functions

--- q5/q5.s
+++ q5/q5.s
@@ -1,83 +1,74 @@
 .section .data
 filename: .asciz "input.txt"
-yes: .asciz "Yes\n"
-no: .asciz "No\n"
+mode: .asciz "r"
+yes: .asciz "Yes"
+no: .asciz "No"
 
 .section .bss
 left: .skip 1
 right: .skip 1
 
 .section .text
-.globl _start
-
+.globl main #to match libc expectations
 
 #lseek(fd, 0, SEEK_END) for the first lseek, then we change it by setting an offset and indicate this with SEEK_SET
 #openat(AT_FDCWD, "input.txt", O_RDONLY, 0)
 
-_start:
+# we use fopen and fseek now instead, and ftell and fgetc for char by char
+# fopen("input.txt", "r")
+# fseek(file, 0, SEEK_END)
 
-    li a7, 56 # sys call openat
-    li a0, -100 #AT_FDCWD
-    la a1, filename
-    li a2, 0 # O_RDONLY
-    li a3, 0
-    ecall
+main:
 
-    bltz a0, exit #error
+    la a0, filename
+    la a1, mode
+    call fopen
+    beqz a0, exit
+    mv s0, a0 #file* essentiallly
 
-    mv s0, a0 #fd is in s0 now
+    #fseek
 
-    #lseek
+    mv a0, s0
+    li a1, 0
+    li a2, 2
+    call fseek
 
-    li a7, 62 #lseek sys call
-    mv a0, s0 #fd
-    li a1, 0 #offset
-    li a2, 2 #SEEK_END
-    ecall
-
-    mv s1, a0 #s1=file size
+    # size = ftell(file)
+    mv a0, s0
+    call ftell
+    mv s1, a0 #size
 
     li s2, 0 #left = 0
     addi s3, s1, -1 #right = size-1
 
 loop:
 
-    bge s2, s3, isPal
+    bgt s2, s3, isPal
     
     # left char
 
-    li a7, 62 #lseek syscall
     mv a0, s0
-    mv a1, s2 #offset = left
-    li a2, 0 #SEEK_SET
-    ecall
+    mv a1, s2
+    li a2, 0
+    call fseek
 
-    li a7, 63 #read
     mv a0, s0
-    la a1, left
-    li a2, 1 #reading 1 byte
-    ecall
+    call fgetc
+    mv t1, a0
+
 
     #right char
-    li a7, 62 #lseek
+    
     mv a0, s0
-    mv a1, s3 #offset = right
+    mv a1, s3
     li a2, 0 #SEEK_SET
-    ecall
+    call fseek 
 
-    li a7, 63 #read
     mv a0, s0
-    la a1, right
-    li a2, 1
-    ecall
+    call fgetc
+    mv t2, a0
 
     #comparing char
-
-    la t0, left
-    lb t1, 0(t0)
-
-    la t0, right
-    lb t2, 0(t0)
 
     bne t1, t2, notPal
 
@@ -87,28 +78,16 @@ loop:
 
 isPal:
 
-    li a7, 64 #write
-    li a0, 1 #stdout
-
-    la a1, yes
-    li a2, 4
-    ecall
-    j exit
+    la a0, yes
+    call puts
+    j done
 
 notPal:
 
-    li a7, 64 #write
-    li a0, 1 #stdout
+    la a0, no
+    call puts
 
-    la a1, no
-    li a2, 3
-    ecall
+done:
 
-exit:
-
-    li a7, 93 #exit
     li a0, 0
-    ecall
-
-
-
+    call exit
